f8e62f790a839b5106de11665cbc8917c29523e8
Graham Ollis <plicease@cpan.org>
2017-09-15 13:08:51 -0400

    Initial commit

---
+++ lib/Alien/Build/Util/Win32/RegistryDump.pm
@@ -0,0 +1,10 @@
+package Alien::Build::Util::Win32::RegistryDump;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Private utility functions for Alien::Build for windows registry
+# VERSION
+
+1;
